dac89fb9929596b942b94cc28c9ac5aab27751d3
Joao Pombo <jpombo2001@hotmail.com>
2025-11-02 11:13:07 -0300

    aa

--- src/app/page.js
+++ src/app/page.js
@@ -53,7 +53,7 @@ const professionals = [
     description1: "Psicóloga clínica pela abordagem psicanalítica.",
     description2:
       "Atendimento de adolescentes e adultos, com foco em processos de autoconhecimento, transformação pessoal e compreensão das próprias dores.",
-    image: "/imgs/fotoBrenda.jpg",
+    image: "/imgs/fotoJulia.jpg",
   },
   {
     name: "Dr. Maicon Lucas Martins",
